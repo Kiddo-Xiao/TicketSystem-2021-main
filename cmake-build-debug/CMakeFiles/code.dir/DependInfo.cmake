98a373456bd1511bacae1a725fef747a347aa004
Kiddo-Xiao <Kiddo_Xiao@sjtu.edu.cn>
2021-06-20 14:31:29 +0800

    submit2

---
+++ cmake-build-debug/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/Connect.cpp" "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/CMakeFiles/code.dir/Connect.cpp.o"
+  "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/OrderRelated.cpp" "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/CMakeFiles/code.dir/OrderRelated.cpp.o"
+  "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/TrainRelated.cpp" "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/CMakeFiles/code.dir/TrainRelated.cpp.o"
+  "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/UserRelated.cpp" "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/CMakeFiles/code.dir/UserRelated.cpp.o"
+  "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/main.cpp" "/mnt/c/Users/11582/Desktop/Computer Scuence/TrainTicket/TicketSystem-2021-main/cmake-build-debug/CMakeFiles/code.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/Connect.cpp.o"
+  "CMakeFiles/code.dir/OrderRelated.cpp.o"
+  "CMakeFiles/code.dir/TrainRelated.cpp.o"
+  "CMakeFiles/code.dir/UserRelated.cpp.o"
+  "CMakeFiles/code.dir/main.cpp.o"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
